492065d9bcbd9d3f3107ea15da521ec4cff4e8e8
Pozharov Roman <ru.roman.ps@gmail.com>
2019-10-02 06:13:44 +0400

    Вынес стор в store.js

---
+++ js/store.js
@@ -0,0 +1,84 @@
+'use strict';
+
+window.app.store = (function(){
+  const store = {}
+  const storeData = [
+    {
+      name: 'inputLength',
+      value: 0,
+      setterHook: calcPriceCheckBtnNext,
+    },
+    {
+      name: 'inputHeight',
+      value: 0,
+      setterHook: calcPriceCheckBtnNext,
+    },
+    {
+      name: 'borderArea',
+      value: 0,
+      setterHook: renderPrice,
+    },
+    {
+      name: 'selectedMaterial',
+      value: 'choose',
+      setterHook: calcPriceCheckBtnNext,
+    },
+    {
+      name: 'checkboxInstalling',
+      value: '',
+      setterHook: calcPriceCheckBtnNext,
+    },
+    {
+      name: 'submitDisabled',
+      value: 0,
+      setterHook: switchBtnNext,
+    },
+    {
+      name: 'switchFormScreens',
+      value: 0,
+      setterHook: renderFormScreen,
+    }
+  ]
+  
+  store.inputLength = window.app.lib.getStoreItem({
+    name: 'inputLength',
+    value: 0,
+    setterHook: calcPriceCheckBtnNext,
+  });
+
+  store.inputHeight = window.app.lib.getStoreItem({
+    name: 'inputHeight',
+    value: 0,
+    setterHook: calcPriceCheckBtnNext,
+  });
+
+  store.borderArea = window.app.lib.getStoreItem({
+    name: 'borderArea',
+    value: 0,
+    setterHook: renderPrice,
+  });
+
+  store.selectedMaterial = window.app.lib.getStoreItem({
+    name: 'selectedMaterial',
+    value: 'choose',
+    setterHook: calcPriceCheckBtnNext,
+  });
+
+  store.checkboxInstalling = window.app.lib.getStoreItem({
+    name: 'checkboxInstalling',
+    value: '',
+    setterHook: calcPriceCheckBtnNext,
+  });
+
+  store.submitDisabled = window.app.lib.getStoreItem({
+    name: 'submitDisabled',
+    value: 0,
+    setterHook: switchBtnNext,
+  });
+
+  store.switchFormScreens = window.app.lib.getStoreItem({
+    name: 'switchFormScreens',
+    value: 0,
+    setterHook: renderFormScreen,
+  })
+})()
